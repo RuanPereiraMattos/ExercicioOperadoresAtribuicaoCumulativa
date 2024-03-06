d5fea7d7b3b3173f1affd61cc22a8ebbd37610f5
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-06 20:23:09 -0300

    Primeira versão com as implementações propostas pelo professor

--- src/main/java/com/ruanmattos/main/ExercicioOperadoresAtribuicaoCumulativa.java
+++ src/main/java/com/ruanmattos/main/ExercicioOperadoresAtribuicaoCumulativa.java
@@ -1,16 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.ruanmattos.main;
 
 /**
+ * Classe principal do Exercício de Operadores de Atribuição Cumulativa 
+ * 01. Incrementar um número em 5
+ * 02. Multiplicar um número por 2
  *
- * @author aluno
+ * @author Ruan Pereira Mattos
  */
 public class ExercicioOperadoresAtribuicaoCumulativa {
 
+    /**
+     * Função padrão do java para iniciar o software
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int soma = 0;
+        soma += 5;
+        System.out.println("Soma: " + soma);
+        int multiplicacao = 1;
+        multiplicacao *= 2;
+        System.out.println("Multiplicação: " + multiplicacao);
     }
 }
